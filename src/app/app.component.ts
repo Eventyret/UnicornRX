e9e14d29a9c54561b66a725ac38ea578844f2aed
Simen Daehlin <simen.dehlin@gmail.com>
2021-01-21 08:14:38 +0000

    Added firebase connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-import { environment } from '@src/environments/environment';
-import { test } from '@app/test';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'UnicornRX';
+  constructor(private afs: AngularFirestore) {}
+  ngOnInit(): void {
+    this.afs
+      .collection('test')
+      .snapshotChanges()
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
 }
